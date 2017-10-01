e7590f25c403cbcc33264568415501071e9f5764
张三丰 <123456@qq.com>
2017-10-01 20:06:21 +0800

    tp5的validate以及场景设置（比如添加、排序等）

--- application/admin/controller/Category.php
+++ application/admin/controller/Category.php
@@ -20,4 +20,19 @@ class Category extends \think\Controller
         //echo('application/admin/controller/Category.php/Category/add');
         return $this->fetch();
     }
+
+    public function save()
+    {
+        //print_r($_POST); //第一种获取数据的方式
+        print_r(input('post.')); //第二种获取数据的方式
+        //print_r(request()->post()); //第三种获取数据的方式
+        //建议使用后面两条，不要用PHP原生的
+        $data = input('post.');
+        $data['status'] = 10; //测试validate
+        $validate = validate('Category');
+        //if (!$validate->check($data)) { //tp5的validate
+        if (!$validate->scene('add')->check($data)) { //验证的场景设置
+            $this->error($validate->getError());
+        }
+    }
 }
