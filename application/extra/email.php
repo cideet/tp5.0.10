813df10528dda454637718d0459597e86d6f8da3
张三丰 <123456@qq.com>
2017-10-06 22:10:14 +0800

    封装phpmainer类库 （测试邮件发送地址：http://127.0.0.64/extend/phpmailer/test/testemail.php ）

---
+++ application/extra/email.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sf
+ * Date: 2017/10/6
+ * Time: 21:35
+ */
+
+return [
+    'host' => 'smtp.126.com',
+    'port' => 25,
+    'username' => 'singwa3@126.com',
+    'password' => 'singwa123',
+];
