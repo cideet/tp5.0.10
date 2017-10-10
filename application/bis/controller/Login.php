777ac9e2d6922d80c6d78e04d76a4251c976bf9d
张三丰 <123456@qq.com>
2017-10-10 23:36:03 +0800

    临时更新

--- application/bis/controller/Login.php
+++ application/bis/controller/Login.php
@@ -14,4 +14,27 @@ class Login extends \app\bis\controller\Basecontroller
     {
         return $this->fetch();
     }
+
+    public function check()
+    {
+        $username = input('post.username');
+        $password = input('post.password');
+        if (!trim($username) || !trim($password)) {
+            return show(0, '用户名和密码不能为空');
+        }
+        $ret = model('Adminuser')->getAdminuserByUsername($username);
+        if (!$ret || $ret['status'] != 1) {
+            return show(0, '该用户不存在');
+        } elseif ($ret['password'] != getVdouwMD5($password)) {
+            return show(0, '密码错误');
+        }
+        session('adminUser', $ret);
+        return show(1, '登录成功');
+    }
+
+    public function logout()
+    {
+        session('adminUser', null);
+        $this->redirect('/index.php/bis/login/index');
+    }
 }
