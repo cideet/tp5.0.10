7c57399ecbc79204e4a8284b4cff261dc2517db6
zhangsanfeng <123456@qq.com>
2017-10-11 10:36:58 +0800

    临时更新

--- application/database.php
+++ application/database.php
@@ -10,12 +10,6 @@
 // +----------------------------------------------------------------------
 
 return [
-
-//    'hostname' => '*',         // 服务器地址
-//    'database' => '*',                    // 数据库名
-//    'username' => '*',                    // 用户名
-//    'password' => '*',    // 密码
-
     'hostname' => 'localhost',
     'database' => 'tp5',
     'username' => 'root',
@@ -23,32 +17,18 @@ return [
 
     'type' => 'mysql',                      // 数据库类型
     'hostport' => '3306',                   // 端口
-    // 连接dsn
-    'dsn' => '',
-    // 数据库连接参数
-    'params' => [],
-    // 数据库编码默认采用utf8
-    'charset' => 'utf8',
-    // 数据库表前缀
-    'prefix' => 'o2o_',
-    // 数据库调试模式
-    'debug' => true,
-    // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
-    'deploy' => 0,
-    // 数据库读写是否分离 主从式有效
-    'rw_separate' => false,
-    // 读写分离后 主服务器数量
-    'master_num' => 1,
-    // 指定从服务器序号
-    'slave_no' => '',
-    // 是否严格检查字段是否存在
-    'fields_strict' => true,
-    // 数据集返回类型
-    'resultset_type' => 'array',
-    // 自动写入时间戳字段
-    'auto_timestamp' => false,
-    // 时间字段取出后的默认时间格式
-    'datetime_format' => 'Y-m-d H:i:s',
-    // 是否需要进行SQL性能分析
-    'sql_explain' => false,
+    'dsn' => '',                            // 连接dsn
+    'params' => [],                         // 数据库连接参数
+    'charset' => 'utf8',                    // 数据库编码默认采用utf8
+    'prefix' => 'o2o_',                     // 数据库表前缀
+    'debug' => true,                        // 数据库调试模式
+    'deploy' => 0,                          // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
+    'rw_separate' => false,                 // 数据库读写是否分离 主从式有效
+    'master_num' => 1,                      // 读写分离后 主服务器数量
+    'slave_no' => '',                       // 指定从服务器序号
+    'fields_strict' => true,                // 是否严格检查字段是否存在
+    'resultset_type' => 'array',            // 数据集返回类型
+    'auto_timestamp' => false,              // 自动写入时间戳字段
+    'datetime_format' => 'Y-m-d H:i:s',     // 时间字段取出后的默认时间格式
+    'sql_explain' => false,                 // 是否需要进行SQL性能分析
 ];
