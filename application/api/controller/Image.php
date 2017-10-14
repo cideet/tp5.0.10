55159ed5f2e9123a92d2ef63af7084adade11583
张三丰 <123456@qq.com>
2017-10-14 12:23:12 +0800

    图片上传

---
+++ application/api/controller/Image.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sf
+ * Date: 2017/10/14
+ * Time: 11:26
+ */
+
+namespace app\api\controller;
+
+class Image extends \think\Controller
+{
+    public function upload()
+    {
+        $file = \think\Request::instance()->file('file');
+        $info = $file->move('upload');
+        if ($info) {
+            return show(1, '上传成功', '/' . $info->getPathname());
+        }
+        return show(0, '上传失败');
+    }
+}
