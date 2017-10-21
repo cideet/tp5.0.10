da967b3e3d0597f36bd771aea62383a9b82360d9
张三丰 <123456@qq.com>
2017-10-21 23:36:35 +0800

    前台功能完善中

--- application/home/controller/Index.php
+++ application/home/controller/Index.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: sf
+ * Date: 2017/10/21
+ * Time: 22:40
+ */
+
 namespace app\home\controller;
 
 class Index extends \app\home\controller\Basecontroller
@@ -18,11 +25,27 @@ class Index extends \app\home\controller\Basecontroller
     public function index()
     {
         $allNews = model('News')->getAllDatas();
+        //echo(json_encode($allNews));
         return $this->fetch('', [
             'memberUsername' => $this->memberUsername,
             'allNews' => $allNews
         ]);
     }
 
+    /**
+     * 文章 内容页
+     * @return mixed
+     */
+    public function article()
+    {
+        $articleId = getParam('id');
+        if (!!$articleId) {
+            return $this->fetch('', [
+                'memberUsername' => $this->memberUsername
+            ]);
+        } else {
+            echo('你丫坑爹呀');
+        }
+    }
 
 }
