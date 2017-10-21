3ff206754ae7eb1158b29fb2e79e47a13db9baea
张三丰 <123456@qq.com>
2017-10-21 17:08:40 +0800

    完善会员注册中

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -9,15 +9,18 @@ class Index extends \think\Controller
     }
 
     /**
-     * 注册
+     * 会员注册
+     * @return array|mixed|void
      */
     public function register()
     {
         $data = input('post.');
         if ($data) {
-            //$data['status'] = 1;
+            if (trim($data['rpassword']) != trim($data['password'])) return show(0, '密码不一致');
+            if (model('Member')->checkUsernameOnly(trim($data['username']))) return show(0, '用户名不唯一');
+            if (model('Member')->checkEmailOnly(trim($data['email']))) return show(0, ',邮件不唯一');
             $data['password'] = getVdouwMD5($data['password']);
-            $resId = model('Oauth')->add($data);
+            $resId = model('Member')->add($data);
             if ($resId) {
                 return show(1, "添加成功", $resId);
             }
