c59c8492f9fda5efda48b63a1954e45f41ba7885
张三丰 <zhangsanfeng@qq.com>
2017-10-27 16:40:30 +0800

    完善 登录前后的cookie和session

--- public/home/js/js.js
+++ public/home/js/js.js
@@ -2,13 +2,15 @@
  * Created by sf on 2017/10/14.
  */
 
+/**
+ * 前端判断是否登录
+ * recordUrl的作用是：记录当前网址，登录后跳转回来
+ * @returns {boolean}
+ */
 function isLogin() {
-    if (vdouwTool.getCookie('memberUserCookie')) {
-        return true;
-    } else {
-        //vdouwTool.setCookie('recordUrl', location.href);
+    if (!vdouwTool.getCookie('memberUserCookie')) {
+        vdouwTool.setCookie('recordUrl', location.href);
         dialog.confirm('请先登录', '/index.php/home/login/index');
-        // dialog.error('请先登录');
-        // return false;
     }
 }
+
