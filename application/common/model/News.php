46fe7cec29c2c2d7ec5387993fcbe8e8518da256
张三丰 <zhangsanfeng@qq.com>
2017-11-01 15:13:11 +0800

    临时更新

--- application/common/model/News.php
+++ application/common/model/News.php
@@ -38,27 +38,36 @@ class News extends \app\common\model\Basemodel
     }
 
     /**
-     * 传递文章ID获取单条全部数据，$map主要为前台页面上下页使用
+     * 传递文章ID获取单条全部数据
      * @param $newsId
      * @param string $map
      * @return array|false|\PDOStatement|string|\think\Model
      */
-    public function getNewsById($newsId, $map = '')
+    public function getNewsById($newsId)
     {
-        if ($map == '') {   // $map为空则不获取上下篇文章
-            $where = ['id' => $newsId];
-            $ret = $this->where($where)->find();
-            return $ret;
-        } else {
-        }
+        $where = ['id' => $newsId];
+        $ret = $this->where($where)->find();
+        return $ret;
     }
 
+    /**
+     * 下一篇
+     * @param $categoryId
+     * @param $thisNewsId
+     * @return array|false|\PDOStatement|string|\think\Model
+     */
     public function getNextNews($categoryId, $thisNewsId)
     {
         $where = ['category_id' => $categoryId, 'id' => array('gt', $thisNewsId)];
         return $this->where($where)->field('id,title')->find();
     }
 
+    /**
+     * 上一篇
+     * @param $categoryId
+     * @param $thisNewsId
+     * @return array|false|\PDOStatement|string|\think\Model
+     */
     public function getPrevNews($categoryId, $thisNewsId)
     {
         $where = ['category_id' => $categoryId, 'id' => array('lt', $thisNewsId)];
