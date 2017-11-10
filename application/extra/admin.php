fd37351804e59da104b239d4b5804f38527c72b2
张三丰 <zhangsanfeng@qq.com>
2017-11-10 10:46:26 +0800

    后台权限控制功能的实现（也就改造一下下）

---
+++ application/extra/admin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sf
+ * Date: 2017/10/14
+ * Time: 21:05
+ */
+    
+return [
+    'myname' => 'zhangsanfeng',
+    'session_userinfo' => 'adminUser',
+    'session_userinfo_scope' => 'adminUser',
+];
