b9c614c290477f2522732f675a4534ab49f58272
张三丰 <zhangsanfeng@qq.com>
2017-11-10 11:40:45 +0800

    临时更新

--- application/api/controller/Image.php
+++ application/api/controller/Image.php
@@ -10,6 +10,10 @@ namespace app\api\controller;
 
 class Image extends \think\Controller
 {
+    /**
+     * 上传缩略图
+     * @return array|void
+     */
     public function upload()
     {
         $file = \think\Request::instance()->file('file');
@@ -20,9 +24,12 @@ class Image extends \think\Controller
         return show(0, '上传失败');
     }
 
+    /**
+     * kingeditor中的图片上传
+     * @return bool|string
+     */
     public function imageUpload() {
         $res = \think\Request::instance()->file('file');
-
         if($res) {
             return '/' . $res['file']['savepath'] . $res['file']['savename'];
         }else{
