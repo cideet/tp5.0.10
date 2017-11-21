b6fbdf6c7efb75add37f979356a97b6282b764c4
张三丰 <zhangsanfeng@qq.com>
2017-11-21 12:23:45 +0800

    React初始化项目-引入路由

--- public/react/src/containers/index.jsx
+++ public/react/src/containers/index.jsx
@@ -4,21 +4,37 @@
 
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import {Link} from 'react-router';
 
-import List from '../components/List/index.jsx';
 
 class Index extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+        this.state = {initDone: false};
     }
 
     render() {
         return (
             <div>
-                <List/>
+                {
+                    this.state.initDone
+                        ? this.props.children
+                        : <div>正在加载...</div>
+                }
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/articlelist">ArticleList</Link></li>
+                    <li><Link to="/notFound">NotFound</Link></li>
+                </ul>
             </div>
         )
+    }
+
+    componentDidMount() {
+        setTimeout(()=> {
+            this.setState({initDone: true});
+        }, 1000);
     }
 }
 
--- public/react/src/router/index.jsx
+++ public/react/src/router/index.jsx
@@ -6,12 +6,19 @@ import React from 'react';
 import {Router, Route, IndexRoute} from 'react-router';
 
 import App from '../containers/index.jsx';
+import Home from '../containers/Home/index.jsx';
+import ArticleList from '../containers/ArticleList/index.jsx';
+import NotFound from '../containers/NotFound/index.jsx';
 
 class Index extends React.Component {
     render() {
         return (
             <Router history={this.props.history}>
-                <Route path="/" component={App}></Route>
+                <Route path="/" component={App}>
+                    <IndexRoute component={Home}/>
+                    <Route path="articlelist" component={ArticleList}/>
+                    <Route path="*" component={NotFound}/>
+                </Route>
             </Router>
         );
     }
